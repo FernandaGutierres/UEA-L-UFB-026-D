f816ffe43af5958d96341de6433148b84c31a471
GutierresFernanda <mf.gutierresa@uea.edu.ec>
2024-03-17 11:55:42 -0500

    sistema actualixado

--- SEMANA14/CalculoDescuentoPython.py
+++ SEMANA14/CalculoDescuentoPython.py
@@ -1,22 +1,24 @@
-def calcular_descuento(monto_total, porcentaje_descuento=20):
-    descuento = (monto_total * porcentaje_descuento) / 50
+def calcular_descuento(monto_total, porcentaje_descuento=10):
+    descuento = (monto_total * porcentaje_descuento) / 100
     return descuento
 
 
 def main():
     # Llamadas a la función calcular_descuento
-    monto_compra_1 = 4000
-    descuento_1 = calcular_descuento(monto_compra_1)
-    monto_final_1 = monto_compra_1 - descuento_1
-    print(f"Monto total de compra: ${monto_compra_1}")
+    compra_1 = 2000
+    descuento_1 = calcular_descuento(compra_1)
+    monto_final_1 = compra_1 - descuento_1
+    print(f"Monto total de compra: ${compra_1}")
     print(f"Descuento aplicado: ${descuento_1}")
     print(f"Monto final a pagar: ${monto_final_1}\n")
 
-    monto_compra_2 = 8000
+    compra_2 = 4000
     porcentaje_descuento_2 = 20
-    descuento_2 = calcular_descuento(monto_compra_2, porcentaje_descuento_2)
-    monto_final_2 = monto_compra_2 - descuento_2
-    print(f"Monto total de compra: ${monto_compra_2}")
+    descuento_2 = calcular_descuento(compra_2, porcentaje_descuento_2)
+    monto_final_2 = compra_2 - descuento_2
+    print(f"Monto total de compra: ${compra_2}")
     print(f"Porcentaje de descuento: {porcentaje_descuento_2}%")
     print(f"Descuento aplicado: ${descuento_2}")
     print(f"Monto final a pagar: ${monto_final_2}")
+
+if __name__ == "__main__": main()
